adddb35b8d741d034a03301637ca1da9d4ac9ad3
Jonasson Oscar <oscjjonasson@gmail.com>
2022-09-20 10:58:16 +0300

    first

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-var Person = function(first, last, middle) {
+var Person = function (first, last, middle) {
   this.first = first;
   this.middle = middle;
   this.last = last;
 };
 
 Person.prototype = {
-
-  whoAreYou : function() {
-    return this.first + (this.middle ? ' ' + this.middle: '') + ' ' + this.last;
-  }
-
+  whoAreYou: function () {
+    return (
+      this.first + (this.middle ? ' ' + this.middle : '') + ' ' + this.last
+    );
+  },
 };
 
-var p1 = Person("Johnny", "James", "", 23);
-
-
+var p1 = Person('Johnny', 'James', '', 23);
